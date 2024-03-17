7105a04e84379f999042d795f61165edaa994fc0
damiththennakoon <134676424+damiththennakoon@users.noreply.github.com>
2024-03-17 20:57:58 +0530

    intilize

---
+++ kdjbolg/src/layouts/MainLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+defineOptions({
+  name: "MainLayout",
+});
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
